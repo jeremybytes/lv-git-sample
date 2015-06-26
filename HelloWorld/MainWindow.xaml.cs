6e56720f9ace196949a7504df10777b774a20a61
Jeremy Clark <jeremy@jeremybytes.com>
2015-06-25 19:52:27 -0700

    Changed to lambda expression

--- HelloWorld/MainWindow.xaml.cs
+++ HelloWorld/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
         {
             InitializeComponent();
             ClickMeButton.Click +=
-                delegate(object sender, RoutedEventArgs e)
+                (object sender, RoutedEventArgs e) =>
                 {
                     MessageBox.Show("Hello World!");
                 };
