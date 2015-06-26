af00f49680ca1fbda54856c652bd7cdf9fbf64b2
Jeremy Clark <jeremy@jeremybytes.com>
2015-06-25 19:33:54 -0700

    Initial project

---
+++ HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HelloWorld
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
